0150a602c59e4cdbff7bd0fb669e4b18c9c9f39d
Prajesh Gupta <imprj.gupta@gmail.com>
2022-04-11 14:35:00 +0530

    rotten tomatoes working initialized

--- src/test/java/com/qt/qualithon/TestSession.java
+++ src/test/java/com/qt/qualithon/TestSession.java
@@ -40,7 +40,7 @@ public class TestSession {
      * @return    webdriver wait instance
      **/
     public WebDriverWait driverWait(){
-        return this.driverWait(30);
+        return this.driverWait(80);
     }
     /**
      * webdriver wait instane with wait timeout set to 90 seconds
@@ -48,7 +48,7 @@ public class TestSession {
      * @return    webdriver wait instance
      **/
     public WebDriverWait driverLongWait(){
-        return this.driverWait(90);
+        return this.driverWait(120);
     }
     /**
      * webdriver wait instane with wait timeout set to 10 seconds
